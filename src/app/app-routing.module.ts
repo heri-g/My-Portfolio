0efdc522c10786eaff29e265a30a03aab9a85348
Heri <heriguti23@gmail.com>
2017-06-25 08:14:46 -0400

    Initialized app
    Initialized app and created 2 components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { FrontPageComponent } from './components/front-page/front-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/front-page', pathMatch: 'full' },
+  { path: 'front-page', component: FrontPageComponent }
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from 'app/app-routing.module';
+
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './components/front-page/front-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
